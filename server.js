5b063f30f1905b66665b439a3ab194fb20e42bf6
Mehdi Bourahla <mehdibrhl4@gmail.com>
2024-09-23 00:14:07 +0100

    🚨 Remove MongoDB warnings

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
